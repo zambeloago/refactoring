9496a2241a100fd7bff589bf00007819df4455da
Loago Zambe <zambeloago@gmail.com>
2025-11-19 22:42:54 -0500

    Finished Task 1.1

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -5,11 +5,28 @@ package theater;
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private String playID;
+    private int audience;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
     }
+
+    public int getAudience() {
+        return audience;
+    }
+
+    public void setAudience(int audience) {
+        this.audience = audience;
+    }
+
+    public String getPlayID() {
+        return playID;
+    }
+
+    public void setPlayID(String playID) {
+        this.playID = playID;
+    }
 }
+
